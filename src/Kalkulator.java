e31dc40b59b83ee9957ea325feeb4c16274f1280
Yohanes Eka Putra <ekaadev9@gmail.com>
2023-12-19 22:30:56 +0700

    refactor(func): operasi menjadi kalkulasi

--- src/Kalkulator.java
+++ src/Kalkulator.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class Kalkulator {
 
     public static void desc() {
-        System.out.println("======================================================");
-        System.out.println("======================================================");
-        System.out.println("Program Kalkulator Sedeerhana");
-        System.out.println("+ : Melakukan Opeasi Penambahan");
-        System.out.println("- : Melakukan Opeasi Pengurangan");
-        System.out.println("* : Melakukan Opeasi Perkalian");
-        System.out.println("/ : Melakukan Opeasi Pembagian");
-        System.out.println("======================================================");
-        System.out.println("======================================================");
+        System.out.println("============================================================================================");
+        System.out.println("Program Kalkulator Sederhana");
+        System.out.println("============================================================================================");
+        System.out.println("Anda dapat menggunakan tanda di bawah ini untuk melakukan berbagai macam operasi matematika");
+        System.out.println("'+' : Melakukan Opeasi Penjumlahan");
+        System.out.println("'-' : Melakukan Opeasi Pengurangan");
+        System.out.println("'*' : Melakukan Opeasi Perkalian");
+        System.out.println("'/' : Melakukan Opeasi Pembagian");
+        System.out.println("============================================================================================");
     }
     public static int bil1(Scanner sc) {
         System.out.print("Masukkan bilangan pertama = ");
@@ -31,37 +31,35 @@ public class Kalkulator {
         return sc.next().charAt(0);
     }
 
-    public static void operasi(int bil1, int bil2, char op) {
-        double hasil = 0;
+    public static void kalkulasi(int bil1, int bil2, char op) {
+        double finalDiv;
+        int finalNotDiv;
 
-        switch(op) {
-            case '+':
-                hasil = bil1 + bil2;
-                break;
-            case '-':
-                hasil = bil1 - bil2;
-                break;
-            case '*':
-                hasil = bil1 * bil2;
-                break;
-            case '/':
-                if (bil2 != 0) {
-                    hasil = (double) bil1 / bil2;
-                } else {
-                    System.out.println("Error, Anda Memasukkan angka 0!");
-                }
-        }
-
-        if ( bil2 != 0 ) {
-            System.out.println("Hasilnya adalah " + hasil);
+        if (op == '+') {
+            finalNotDiv = bil1 + bil2;
+            System.out.println("Hasil penjumlahan dari 2 bilangan tersebut = " + finalNotDiv);
+        } else if (op == '-') {
+            finalNotDiv = bil1 - bil2;
+            System.out.println("Hasil pengurangan dari 2 bilangan tersebut = " + finalNotDiv);
+        } else if (op == '*') {
+            finalNotDiv = bil1 * bil2;
+            System.out.println("Hasil perkalian dari 2 bilangan tersebut = " + finalNotDiv);
+        } else if (op == '/') {
+            if (bil2 == 0) {
+                System.out.println("Error, anda memasukkan angka 0!");
+            } else {
+                finalDiv = (double) bil1 / bil2;
+                System.out.printf("Hasil pembagian dari 2 bilangan tersebut = %.2f\n", finalDiv);
+            }
         }
     }
 
     public static int mainLagi(Scanner sc) {
-        System.out.println("======================================================");
-        System.out.println("Apakah anada ingin menncoba lagi (0/1) ? ");
+        System.out.println("============================================================================================");
+        System.out.println("Apakah anda ingin menncoba lagi (0/1) ? ");
         System.out.println("0 : Tidak");
         System.out.println("1 : Ya");
+        System.out.println("============================================================================================");
         System.out.print("Pilihanmu = ");
 
         return sc.nextInt();
@@ -79,7 +77,7 @@ public class Kalkulator {
             int inputBil2 = bil2(sc);
             char inputOp = operator(sc);
 
-            operasi(inputBil1, inputBil2, inputOp);
+            kalkulasi(inputBil1, inputBil2, inputOp);
 
             int inputMainLagi = mainLagi(sc);
 
